39eca3e6715b8a845b334b462096bf2ea2d8a29a
Takeshi Camilo Ramirez Escobar <60365530+LilSherlock@users.noreply.github.com>
2020-06-16 20:32:42 -0500

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 
-int _islower(int c)
+/**
+ * _islower - checks whether the character is a lower case letter
+ * @c: is the character to check
+ *
+ * Return: 1 if the character is lowercase and 0 if it is not lowercase
+ */
+int _islower(char c)
 {
-    if (c >= 97 && c <= 122)
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if ((c >= 97) && (c <= 122))
+		return (1);
+	else
+		return (0);
 }
