ae740899e244f011f745dcf997e033bf7c0986f1
Lilsherlock <potatogames15@gmail.com>
2020-06-19 05:30:50 +0000

    update

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,22 +6,19 @@ void print_square(int size)
 {
 	int top = 0, tap = 0, numeral = 35;
 
-	if (size != 0)
-	{
-		while (top < size)
-		{
-			while (tap < size)
-			{
-				_putchar(numeral);
-				tap++;
-			}
-			tap = 0;
-			_putchar('\n');
-			top++;
-		}
-	}
-	else
+	if (size == 0 || size < 0)
 	{
 		_putchar('\n');
+	}
+	while (top < size)
+	{
+		while (tap < size)
+		{
+			_putchar(numeral);
+			tap++;
+		}
+		tap = 0;
+		putchar('\n');
+		top++;
 	}
 }
