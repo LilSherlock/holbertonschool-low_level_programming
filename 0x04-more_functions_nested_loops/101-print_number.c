0de59853f149723ebbec487dd34527d72f9436f7
Lilsherlock <potatogames15@gmail.com>
2020-06-23 02:53:12 +0000

    update

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,69 @@
+#include "holberton.h"
+/**
+* print_number - main
+* @n: int
+*/
+void print_number(int n)
+{
+	int digit = 0, secondDigit = 0, activar = 0;
+
+	if (n < 0)
+	{
+		n = n * (-1);
+		activar = 1;
+	}
+	if (activar == 1)
+	{
+		_putchar('-');
+	}
+	if (n < 10)
+	{
+		_putchar(n + '0');
+	}
+	else if (n < 100)
+	{
+		digit = firstDigit(n);
+		_putchar(digit + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n < 1000)
+	{
+		digit = n / 10;
+		secondDigit = digit % 10;
+		digit = digit / 10;
+		_putchar (digit + '0');
+		_putchar(secondDigit + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n < 10000)
+	{
+		morThat(n);
+	}
+}
+/**
+* firstDigit - main
+* @a: int
+* Return: a
+*/
+int firstDigit(int a)
+{
+	return (a / 10);
+}
+/**
+* morThat - main
+* @n: int
+*/
+void morThat(int n)
+{
+	int digit = 0, secondDigit = 0, thirdDigit = 0;
+
+		digit = n / 10;
+		secondDigit = digit % 10;
+		digit = digit / 10;
+		thirdDigit = digit % 10;
+		digit = digit / 10;
+		_putchar(digit + '0');
+		_putchar(thirdDigit + '0');
+		_putchar(secondDigit + '0');
+		_putchar(n % 10 + '0');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -6,13 +6,17 @@
 int _isupper(int c);
 int _isdigit(int c);
 int _putchar(char c);
-
 int mul(int a, int b);
+int firstDigit(int a);
+
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_number(int n);
 void print_square(int size);
 void print_triangle(int size);
+void morThat(int n);
+
 #endif
