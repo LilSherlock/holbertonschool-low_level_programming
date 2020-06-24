aa71ac8c624c7322aa45c81765af4248f3ed891e
Lilsherlock <potatogames15@gmail.com>
2020-06-24 03:10:51 +0000

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,7 +6,17 @@
 void puts_half(char *str)
 {
 	int len = _strlen(str);
-	int middle = len / 2;
+	int middle;
+
+	if (len % 2 == 0)
+	{
+		middle = len / 2;
+	}
+	else if (len % 2 != 0)
+	{
+		middle = len - 1;
+		middle = middle / 2;
+	}
 
 	while (str[middle] != '\0')
 	{
