84157517124768e409e6158f9370d84a1922eee4
Lilsherlock <potatogames15@gmail.com>
2020-06-24 03:33:12 +0000

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,32 +1,5 @@
 #include "holberton.h"
 /**
- * puts_half - main
- * @str: char
- */
-void puts_half(char *str)
-{
-	int len = _strlen(str);
-	int middle;
-
-	if (len % 2 == 0)
-	{
-		middle = len / 2;
-	}
-	else if (len % 2 != 0)
-	{
-		middle = len - 1;
-		middle = middle / 2;
-	}
-
-	while (str[middle] != '\0')
-	{
-		_putchar(str[middle]);
-		middle++;
-	}
-	_putchar('\n');
-}
-
-/**
 * _strlen - main
 * @s: char
 * Return: j
@@ -40,4 +13,37 @@ int _strlen(char *s)
 		j++;
 	}
 	return (j);
+}
+
+/**
+ * puts_half - main
+ * @str: char
+ */
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+	int middle;
+	int activador = 0;
+
+	if (len % 2 == 0)
+	{
+		middle = len / 2;
+	}
+	else if (len % 2 != 0)
+	{
+		middle = len - 1;
+		middle = middle / 2;
+		activador = 1;
+	}
+
+	while (str[middle] != '\0')
+	{
+		_putchar(str[middle]);
+		middle++;
+	}
+	if (activador == 1)
+	{
+		str[middle] = '\0';
+	}
+	_putchar('\n');
 }
