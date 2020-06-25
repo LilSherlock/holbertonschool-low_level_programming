66a91529743c2b0a3f06cb5ffdc1b1ad326cfb09
Lilsherlock <potatogames15@gmail.com>
2020-06-25 22:35:49 +0000

    update

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * string_toupper - main
+ * @s: char
+ * Return: s
+ */
+char *string_toupper(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		if (s[x] > 'a'  && s[x] < 'z')
+		{
+			s[x] = s[x] - 32;
+		}
+		x++;
+	}
+	return (s);
+}
