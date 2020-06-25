5c7314247e5252d3bba980c1ad293b488fbcae5f
Lilsherlock <potatogames15@gmail.com>
2020-06-25 23:16:00 +0000

    clean

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * cap_string - main
+ * @s: char
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+	int activador = 0, x = 0;
+
+	while (s[x] != '\0')
+	{
+		if (activador == 1)
+		{
+			if (s[x] >= 'a'  && s[x] <= 'z')
+			{
+				s[x] = s[x] - 32;
+			}
+			activador = 0;
+		}
+		if (s[x] == 32 || s[x] == '\n' || s[x] == '\t'
+		|| s[x] == 46 || s[x] == 33 || s[x] == 63
+		|| s[x] == 34 || s[x] == 40 || s[x] == 41
+		|| s[x] == 123 || s[x] == 125)
+		{
+			activador = 1;
+		}
+		x++;
+	}
+	return (s);
+}
