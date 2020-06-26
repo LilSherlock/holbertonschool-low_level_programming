df124b991770ec315f62a059f3056811efa8de3c
Lilsherlock <potatogames15@gmail.com>
2020-06-26 02:46:33 +0000

    clean

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,36 +1,32 @@
 #include "holberton.h"
-/**
- * leet - main
- * @s: char
- * Return: s
- */
-char *leet(char *s)
-{
-	int x = 0, a = 4, e = 3, o = 0, t = 7, l = 1;
 
-	while (s[x] != '\0')
+/**
+ * leet - encodes a string into 1337
+ * @a: Pointer that points to a string
+ * Return: A string
+**/
+
+char *leet(char *a)
+{
+	int i, j, k;
+	char alpha[] = "oOlLeEaAtT";
+	char replace[] = "0011334477";
+
+	i = 0;
+	while (a[i] != '\0')
 	{
-		if (s[x] == 'a' || s[x] == 'A')
+		j = 0;
+		k = 0;
+		while (alpha[j] != '\0')
 		{
-			s[x] = '0' + a;
+			if (a[i] == alpha[j])
+			{
+				k = j;
+				a[i] = replace[k];
+			}
+			j++;
 		}
-		else if (s[x] == 'e' || s[x] == 'E')
-		{
-			s[x] = '0' + e;
-		}
-		else if (s[x] == 'o' || s[x] == 'O')
-		{
-			s[x] = '0' + o;
-		}
-		else if (s[x] == 't' || s[x] == 'T')
-		{
-			s[x] = '0' + t;
-		}
-		else if (s[x] == 'l' || s[x] == 'L')
-		{
-			s[x] = '0' + l;
-		}
-		x++;
+	i++;
 	}
-	return (s);
+	return (a);
 }
