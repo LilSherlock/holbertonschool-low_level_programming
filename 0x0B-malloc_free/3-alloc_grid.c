14f311e3d1ea62f0b71eb62a346df5d033bdcac1
Lilsherlock <potatogames15@gmail.com>
2020-07-08 01:25:02 +0000

    update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,13 +11,14 @@ int **alloc_grid(int width, int height)
 
 	if (width < 0 || width == 0 || height < 0 || height == 0)
 		return (NULL);
-	buffer = malloc(height * sizeof(int *) + 1);
+	buffer = malloc(height * sizeof(int *));
 	if (buffer == NULL)
 		return (NULL);
 	while (x < height)
 	{
 		buffer[x] = malloc(width * sizeof(int));
 		if (buffer[x] == NULL)
+			free(buffer);
 			return (NULL);
 		i = 0;
 		while (i < width)
