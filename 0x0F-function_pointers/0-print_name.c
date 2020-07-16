e8a6dfc279aed23921262176d23921fa11f1efd7
Lilsherlock <potatogames15@gmail.com>
2020-07-16 03:40:09 +0000

    update

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - main
+ * @name: char
+ * @f: char *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name)
+	{
+		f(name);
+	}
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - main
+ * @array: int
+ * @size: size_t
+ * @action: int
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+
+	if (array && size)
+	{
+		for (x = 0; x < size; x++)
+		{
+			action(array[x]);
+		}
+	}
+}
