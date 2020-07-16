933611db2d239bdbda4a305926bfc0c062875d76
Lilsherlock <potatogames15@gmail.com>
2020-07-16 04:43:29 +0000

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,14 +1,5 @@
 #ifndef HOLBY
 #define HOLBY
-<<<<<<< HEAD
-#include <stddef.h>
-
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
-#endif
-=======
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -16,5 +7,4 @@ int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif 
->>>>>>> 98952a836eb1042f4749c6cd0bf1aeb89e5ec45c
+#endif
