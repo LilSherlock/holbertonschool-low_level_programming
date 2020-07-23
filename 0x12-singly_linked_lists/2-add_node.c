56cada219ff5f01b5212efd1569e82895dd1740c
Lilsherlock <potatogames15@gmail.com>
2020-07-23 22:13:14 +0000

    update

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *new;
+    int len = strlen(str);
+
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
+    new->str = _strdup(str);
+    new->len = len;
+    new->next = *head;
+    *head = new;
+    return (new);
+}
+/**
+ * *_strdup - function with one argument
+ * @str: string argument
+ *
+ * Description: returns a pointer to allocated space in memory
+ * Return: pointer
+ */
+char *_strdup(const char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	ptr = malloc(sizeof(char) * i + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
