8d386bf1474cbe98e824decc718b7654e6111fcb
Lilsherlock <potatogames15@gmail.com>
2020-07-29 03:36:38 +0000

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,27 +1,32 @@
 #include "lists.h"
-
+/**
+ * add_nodeint_end - main
+ * @head: listin_t
+ * @n: int
+ * Return: new
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new, *p;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        return (NULL);
-    }
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 
-    new->n = n;
-    new->next = NULL;
-    if (*head == NULL)
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
-    p = *head;
-    while (p->next != NULL)
-    {
-        p = p->next;
-    }
-    p->next = new;
-    return(new);
+	p = *head;
+	while (p->next != NULL)
+	{
+		p = p->next;
+	}
+	p->next = new;
+	return (new);
 }
