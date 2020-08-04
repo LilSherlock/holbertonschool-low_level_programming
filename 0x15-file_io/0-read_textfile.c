c52408be85749b8ade5ef4d25ef6183d979f25b8
lilsherlock <potatogames15@gmail.com>
2020-08-04 15:38:43 -0500

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,17 +7,34 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buff[letters];
-	int fd = open(filename, O_RDONLY);
-	int x;
+	char *buff;
+	int x = 0, fd = 0, output = 0;
 
-	if (fd == -1 || filename == NULL)
+	if (filename == NULL)
 		return (0);
+	buff = malloc(letters);
 
+	if (buff == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buff);
+		return (0);
+	}
 	x = read(fd, buff, letters);
-	close(fd);
 	if (x == 0)
+	{
+		free(buff);
 		return (0);
-	printf("%s", buff);
-	return (1);
+	}
+	output = write(STDOUT_FILENO, buff, x);
+	if (output == -1)
+	{
+		free(buff);
+		exit(0);
+	}
+	close(fd);
+	free(buff);
+	return (output);
 }
