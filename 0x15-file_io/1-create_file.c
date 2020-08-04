9712e571a3ef3e0eb23830c68b34f78e43cb7ed7
lilsherlock <potatogames15@gmail.com>
2020-08-04 16:19:17 -0500

    update

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * create_file - name
+ * @filename: char
+ * @text_content: char
+ * Return: 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0, writting = 0, x = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[x] != '\0')
+			x++;
+		writting = write(fd, text_content, x);
+	}
+	if (writting == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
