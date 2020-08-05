17fb957f40a5e11b6e2b95f1b4f8aff50966b521
lilsherlock <potatogames15@gmail.com>
2020-08-04 23:08:11 -0500

    update

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int fd = 0, writ = 0, x = 0;
+
+    if (filename == NULL)
+        return (-1);
+    if (text_content == NULL)
+        return (-1);
+    fd = open(filename, O_WRONLY | O_APPEND);
+    if (fd == -1)
+        return (-1);
+    if (text_content)
+    {
+        while(text_content[x] != '\0')
+            x++;
+        writ = write(fd, text_content, x);
+    }
+    if (writ == -1)
+        return (-1);
+    close(fd);
+    return(1);
+}
