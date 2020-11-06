7768d44a1e1086581f11afd55007fe29e2b4d846
lilsherlock <potatogames15@gmail.com>
2020-11-06 16:55:16 -0500

    update

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""5-island_perimeter module"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    counter = 0
+    top = False
+
+    for index in range(len(grid)):
+        for sub in range(len(grid[index])):
+            if grid[index][sub] == 1:
+                if grid[index - 1][sub] == 0:
+                    counter += 1
+                if grid[index][sub - 1] == 0:
+                    counter += 1
+                if grid[index][sub + 1] == 0:
+                    counter += 1
+                if grid[index + 1][sub] == 0:
+                    counter += 1
+    return counter
